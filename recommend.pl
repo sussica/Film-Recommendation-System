55e7199f50e3688c0f6b5862206ccc3e84081718
sussica <sussicaya@gmail.com>
2021-04-11 01:57:36 +0800

    add more questions

--- recommend.pl
+++ recommend.pl
@@ -1,12 +1,21 @@
 % Try: start.
 % Note: each answer needs to end in '.'
 start :-
+    write('   Hello!  '),
+    write('Welcome to the film recommendation system. Please answer questions below!'),nl,
     ask1, nl,
     read(Ans1), nl,
     ask2, nl, 
     read(Ans2), nl,
+    ask3, nl, 
+    read(Ans3), nl,
+    ask4, nl,
+    read(Ans4), nl,
+    ask5, nl,
+    read(Ans5), nl,
     % print the first 100 matched films
-    forall(limit(100, distinct(recommend(Ans1, Ans2, F))), writeln(F)).
+    write('we recommend following film:'), nl,
+    forall(limit(100, distinct(recommend(Ans1, Ans2, Ans3, Ans4, Ans5, F))), writeln(F)).
 
  
 ask1 :- 
@@ -24,6 +33,26 @@ ask2 :-
     write("5. Funny"), nl,
     write("0. No preference"). 
 
+ask3 :-
+    write("Do you have long time for the movie? "), nl,
+    write("1. Yes"),nl, 
+    write("2. No"),nl. 
+
+
+ask4 :-
+    write("Which country's movie do you prefer to watch? "), nl,
+    write("1. US"),nl, 
+    write("2. UK"),nl,
+    write("3. Canada"),nl,
+    write("4. Japan"),nl,
+    write("5. Korea"),nl,
+    write("6. China"),nl,
+    write("0. No preference"),nl.
+
+ask5 :-
+    write("Do you want to watch movies that only high score? "), nl,
+    write("1. Yes"),nl, 
+    write("2. Doesn't matter"),nl.
 
 % q1(0, _) is always true since no preference
 q1(0, _). 
@@ -56,12 +85,44 @@ q2(5, ID) :-
     db(ID, genre, 'comedy'). 
 
 
+% q3(1, ID) is true if the runtime is larger than 100.
+q3(1, ID) :-
+    db(ID, runtime, R),
+    R >= 100.
+% q3(2, ID) is true if the runtime is smaller than 100.
+q3(2, ID) :-
+    db(ID, runtime, R),
+    R < 100.
+
+q4(0. _).
+q4(1, ID) :-
+    db(ID, coutry, 'US').
+q4(2, ID) :-
+    db(ID, coutry, 'UK').
+q4(3, ID) :-
+    db(ID, coutry, 'Canada').
+q4(4, ID) :-
+    db(ID, coutry, 'Japan').
+q4(5, ID) :-
+    db(ID, coutry, 'Korea').
+q4(6, ID) :-
+    db(ID, coutry, 'China').
+
+% q5(1, ID) is true if the score is larger than 7.5.
+q5(1, ID) :-
+    db(ID, score, S),
+    S >= 7.5.
+q5(2, _).
+
 % Try: recommend(2, 1, F). -- returns all matches to modern emotional films
 % TODO: to add more questions, simply add another parameter and define all possible qn()'s for that question
-recommend(Ans1, Ans2, Filmname) :-
+recommend(Ans1, Ans2, Ans3, Filmname) :-
     db(ID, name, Filmname), 
     q1(Ans1, ID),
-    q2(Ans2, ID). 
+    q2(Ans2, ID),
+    q3(Ans3, ID),
+    q4(Ans4, ID),
+    q5(Ans5, ID).
 
 
 % Example knowledge base of films: 
